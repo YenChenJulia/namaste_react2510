34b0bc27dcd6160da18808df83eff1e19858120f
Julia <fairy750122@gmail.com>
2025-10-21 09:32:34 +0800

    header draft

--- App.js
+++ App.js
@@ -1,2 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://www.shutterstock.com/image-vector/mobile-accessories-shop-logo-design-260nw-1971174995.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return <div className="app">{<Header />}</div>;
+};
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
