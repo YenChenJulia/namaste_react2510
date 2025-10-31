5304a9844c2d7b7ee7ffe1663ad83d9aade39d5f
Julia <fairy750122@gmail.com>
2025-10-31 18:31:10 +0800

    add rating element in product-card

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,10 +1,11 @@
 const ProductCard = ({ product }) => {
-  const { name, price, ingredients } = product;
+  const { name, price, ingredients, rating } = product;
   return (
     <div className="product-card" style={{ backgroundColor: "aliceblue" }}>
       <h3>{name}</h3>
       <h4>{ingredients?.join(",")}</h4>
       <h4>NT${price}</h4>
+      <h4>rating:{rating}</h4>
     </div>
   );
 };
