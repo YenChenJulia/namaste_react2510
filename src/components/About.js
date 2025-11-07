1b43baa65276f837452de2f67199a841f2463fb2
Julia <fairy750122@gmail.com>
2025-11-07 13:50:09 +0800

    explain life cycle in class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,37 @@
 import UserCard from "./userCard";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>this is About page</h1>
-      <UserCard name={"aaaaa"} />
-      <UserClass name={"bbbb"} location={"tainan"} />
-    </div>
-  );
-};
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>this is About page</h1>
+//       <UserCard name={"aaaaa"} />
+//       <UserClass name={"bbbb"} location={"tainan"} />
+//     </div>
+//   );
+// };
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent component did mount");
+  }
+  render() {
+    console.log("parent render");
+
+    return (
+      <div>
+        <h1>this is About page</h1>
+        <UserClass name={"aaaa"} location={"tainan"} />
+        <UserClass name={"bbbb"} location={"taipei"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,13 +1,37 @@
-import React from "react";
+import { Component } from "react";
 
-class UserClass extends React.Component {
+class UserClass extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 1,
+    };
+    console.log(this.props.name + "constructor");
   }
+
+  componentDidMount() {
+    console.log(this.props.name + "component did mount");
+  }
+
   render() {
+    console.log(this.props.name+ "render");
+
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>count: {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          count increase
+        </button>
+        <h1>count: {count2}</h1>
         <h1>name: {name}</h1>
         <h3>location: {location}</h3>
       </div>
