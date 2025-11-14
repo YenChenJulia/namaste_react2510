c2b486d61745b1751666d6b8f33cafcdbaf48768
Julia <fairy750122@gmail.com>
2025-11-14 09:56:53 +0800

    build: configure Jest and Babel for React testing

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
+const { runtime } = require("./jest.config");
+
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
 };
