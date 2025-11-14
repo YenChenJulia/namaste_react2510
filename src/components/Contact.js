079bda1a377b46d0d8bc3c15b345662831554171
Julia <fairy750122@gmail.com>
2025-11-14 09:58:44 +0800

    "test: add unit tests for Contact component rendering"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,29 @@
 const Contact = () => {
   return (
     <div>
-      <h1>this is Contact page</h1>
+      <h1 className="font-bold text-3xl m-2 p-2">this is Contact page</h1>
+      <form action="">
+        <input
+          className="border border-black p-2 m-2"
+          type="text"
+          name="name"
+          id=""
+          placeholder="name"
+        />
+        <input
+          className="border border-black p-2 m-2"
+          type="email"
+          name="email"
+          id=""
+          placeholder="email"
+        />
+        <button
+          className="bg-gray-200 p-2 m-2 cursor-pointer rounded-lg"
+          type="submit"
+        >
+          submit
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/__test__/Contact.test.js
@@ -0,0 +1,19 @@
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("should load 2 input boxes on the Contact comp", () => {
+  render(<Contact />);
+  // Querying
+  const inputBoxes = screen.getAllByRole("textbox");
+  // Assertion
+  expect(inputBoxes.length).toBe(2);
+});
